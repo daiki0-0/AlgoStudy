093d818a43dcf07a83f8e956b2ceae4646a40e1c
cuteg <cuteg@DESKTOP-HD18SI9>
2022-12-14 09:05:21 +0900

    초기 커밋.

---
+++ Player.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Pos
+    {
+        public Pos(int y,int x) { Y = y; X = x; }
+        public int Y;
+        public int X;
+    }
+    internal class Player
+    {
+        public int PosY { get; private set; }
+        public int PosX { get; private set; }
+        Random _random = new Random();
+
+        Board _board;
+
+        enum Dir
+        {
+            Up,
+            Left,
+            Down,
+            Right
+        }
+
+        int _dir = (int)Dir.Up;
+        List<Pos> _points = new List<Pos>();
+
+        public void Initialize(int posY, int posX , Board board)
+        {
+            PosY = posY;
+            PosX = posX;
+
+            _board = board;
+
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+            bool[,] found = new bool[ _board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY,PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while ( q.Count> 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for ( int i=0; i<4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+                    if (nextX < 0 || nextX >= _board.Size || nextY <0 || nextY >= _board.Size)
+                        continue;
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+            while (parent[y,x].Y != y || parent[y,x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
+        }
+
+        public void Righthand() // [ 우수법 ]
+        {
+            int[] frontY = new int[] { -1, 0, 1, 0 };
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+            int[] rightY = new int[] { 0, -1, 0, 1 };
+            int[] rightX = new int[] { 1, 0, -1, 0 };
+
+            _points.Add(new Pos(PosY, PosX));
+
+            while (PosX != _board.DestX || PosY != _board.DestY)
+            {
+                // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+                {
+                    // 오른쪽 방향으로 90도 회전
+                    _dir = (_dir - 1 + 4) % 4;
+                    // 앞으로 1보 전진
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                {
+                    // 앞으로 한 보 전진
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else
+                {
+                    // 왼쪽 방향으로 90도 회전
+                    _dir = (_dir + 1 + 4) % 4;
+                }
+            }
+        }
+
+        const int MOVE_TICK = 10;
+        int _sumTick = 0;
+        int _lastIndex = 0;
+        public void Update(int deltaTick)
+        {
+            if (_lastIndex >= _points.Count)
+                return;
+
+            _sumTick += deltaTick;
+            if( _sumTick >= MOVE_TICK )
+            {
+                _sumTick = 0;
+
+                PosY = _points[_lastIndex].Y;
+                PosX = _points[_lastIndex].X;
+                _lastIndex++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Board board = new Board();
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while  (true)
+            {
+                #region 프레임관리
+                //만약에 경과한 시간이 1/30초보다 작다면
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                #endregion
+
+                player.Update(deltaTick);
+
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        
+        }
+    }
+}
